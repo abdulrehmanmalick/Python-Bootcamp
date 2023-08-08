f01dc7331ca7c1798a20adf67993104b6c3af22c
Abdul Rehman <abdworks75@gmail.com>
2023-08-08 14:24:11 +0500

    Day-4 Final Commit

--- Day-4/rockpaperscissor.py
+++ Day-4/rockpaperscissor.py
@@ -4,7 +4,6 @@ import random
 choiceList = ['Rock', 'Paper', 'Scissor']
 
 
-# choice = input('Rock Paper or Scissor? \n')
 
 randChoice = len(choiceList)
 
@@ -12,3 +11,29 @@ random_choice = random.randint(0, randChoice - 1)
 choice = choiceList[random_choice]
 
 
+print("Computer's Choice", choice)
+
+
+Humanchoice = input('Rock Paper or Scissor? \n')
+print(choice)
+
+if Humanchoice == 'Rock' and choice == 'Rock':
+    print('Draw')
+elif Humanchoice == 'Rock' and choice == 'Paper':
+    print('Computer Wins!')
+elif Humanchoice == 'Rock' and choice == 'Scissor':
+    print("You Win!")
+
+if Humanchoice == 'Paper' and choice == 'Paper':
+    print('Draw')
+elif Humanchoice == 'Paper' and choice == 'Scissor':
+    print('Computer Wins!')
+elif Humanchoice == 'Paper' and choice == 'Rock':
+    print("You Win!")
+
+if Humanchoice == 'Scissor' and choice == 'Scissor':
+    print('Draw')
+elif Humanchoice == 'Scissor' and choice == 'Rock':
+    print('Computer Wins!')
+elif Humanchoice == 'Scissor' and choice == 'Paper':
+    print("You Win!")
