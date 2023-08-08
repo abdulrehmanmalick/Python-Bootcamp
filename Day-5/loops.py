9cd0dbded55caee2d81c3c13c2e0c62529bf8a35
Abdul Rehman <abdworks75@gmail.com>
2023-08-08 15:33:38 +0500

    Day-5 Commit

--- Day-5/loops.py
+++ Day-5/loops.py
@@ -5,4 +5,24 @@ for fruit in fruits:
     print(fruit)
     print(fruit + ' Pie')
 
-    
+print('\n')
+
+# For Loop With Range
+for number in range(1,10):
+    print(number)
+
+print('\n')
+
+for number in range(1,10,3):
+    print(number)
+
+print('\n')
+
+
+total = 0
+for number in range(1,100):
+    total += number
+print(total)
+
+
+
