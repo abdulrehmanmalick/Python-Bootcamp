56046df059e830a5b1e3ee3166220731365fa486
Abdul Rehman <abdworks75@gmail.com>
2023-08-15 00:55:19 +0500

    Day-8 First Commit

---
+++ Day-8/functions.py
@@ -0,0 +1,25 @@
+# Simple Function
+def greet():
+    print('Hello')
+    print('I am learning coding')
+    print('Python looks fun')
+
+greet()
+
+# Function that allows for input
+def greeting(name):
+    print(f'Hello {name}')
+    print(f'How do you do {name}?')
+    print(f'Is {name} your name?')
+greeting('abd')
+
+def addition(x,y):
+    sum = x + y
+    print(sum)
+addition(1,3) 
+
+def greet_with(name, location):
+    print(f'Hello {name}')
+    print(f'Hey {name}! Should we meet at {location}?')
+greet_with('Abdul Rehman', 'Chaivinist')
+
