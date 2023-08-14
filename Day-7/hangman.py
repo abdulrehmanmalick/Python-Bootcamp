14689ac1e93b72fcecba1f9ac3cafc8129e79c05
Abdul Rehman <abdworks75@gmail.com>
2023-08-14 21:46:47 +0500

    Final-Day7

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -1,1 +1,57 @@
-# To Do.
+import random
+from hangmanext import word_list
+from hangmanext import display
+from hangmanext import guessed_letters
+
+print('Welcome to Hangman')
+
+for word in word_list:
+    chosen_word = random.choice(word_list)
+
+for _ in chosen_word:
+    display.append('_') 
+
+word_length = len(chosen_word)
+
+# For Testing 
+print(f'The Chosen Word is {chosen_word}')
+print(display)
+
+endGame = False
+lives = 5
+
+while not endGame:
+    guess = input('Enter your character \n').lower()
+
+    print(f'The character you guessed is: {guess}')
+
+    if guess in guessed_letters:
+        print('You already have tried guessing this\n')
+        lives -= 1
+        if lives == 0:
+            endGame = True
+            print('You Lose.')
+    else:
+        guessed_letters.append(guess)
+     
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter  
+
+    if guess not in chosen_word:
+        print('You Guessed it Wrong\n')
+        lives -= 1
+        if lives == 0:
+            endGame = True
+            print('You Lose.')
+
+    print(f'You have now {lives} lives left \n')
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        endGame = True
+        print("You win.")
+
---
+++ Day-7/hangmanext.py
@@ -0,0 +1,5 @@
+word_list = [
+'abruptly','absurd','abyss','affix','askew','avenue','awkward','axiom','azure','bagpipes','bandwagon''banjo','bayou','beekeeper','bikini','blitz','blizzard','boggle','bookworm','boxcar','boxful','buckaroo','buffalo','buffoon','buxom','buzzard','buzzing','buzzwords','caliph','embezzle','equip','espionage','euouae','exodus','faking','fishhook','fixable','fjord','flapjack','flopping','fluffiness','flyby']
+
+display = []
+guessed_letters = []
