8ecf2164c1b2aac6d368763c6944ece8e915080f
Abdul Rehman <abdworks75@gmail.com>
2023-08-17 19:38:29 +0500

    functions

---
+++ Day-10/functions2.py
@@ -0,0 +1,14 @@
+# When a function has more than one return
+
+
+
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name(input('What is your first name? \n'), input('What is your last name? \n')))
+
