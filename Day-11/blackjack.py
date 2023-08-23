e1602d639c3f7f6adc9b2532a47f3e1d29c6c138
Abdul Rehman <abdworks75@gmail.com>
2023-08-23 17:52:19 +0500

    Blackjack

---
+++ Day-11/blackjack.py
@@ -0,0 +1,5 @@
+# BlackJack is a game played using cards
+# Goal of the game is to add the cards to the largest number without going over 21. If the Cards added together and their value go above 21 it is called a BUST and it means you lose immediately
+# J-Q-K -> Counted as 10
+# cards 2 to 10 -> Face value
+# Ace -> 1 it can be either counted as 11 or 1. If ur total is going over 21 then u can count it as 1. else u can count it as 11
